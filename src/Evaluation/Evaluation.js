e62a522c2b719808d083d1f0ae8dbbd656db06a0
krt8 <kurtlawrence.b.manandig@isu.edu.ph>
2026-02-13 10:33:50 +0800

    inayos ko po yung sa navbar and appbar

--- src/Evaluation/Evaluation.js
+++ src/Evaluation/Evaluation.js
@@ -1,10 +1,17 @@
-import { Paper, Typography } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
+import './Page.css';
 
 function Evaluation() {
     return(
-        <Paper>
-            <Typography variant="h6">Evaluation</Typography>
-        </Paper>
+        <Box className="page-container">
+            <Box className="page-header">
+                <Typography variant="h4" className="page-title-main">Transactions</Typography>
+                <Typography variant="h5" className="page-title-sub">Evaluation</Typography>
+            </Box>
+            <Box className="card-container">
+                {/* Content goes here */}
+            </Box>
+        </Box>
     );
 }
 export default Evaluation;
--- src/Evaluation/ListofApplicants.js
+++ src/Evaluation/ListofApplicants.js
@@ -1,10 +1,17 @@
-import { Paper, Typography } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
+import './Page.css';
 
 function ListofApplicants() {
     return(
-        <Paper>
-            <Typography variant="h6">List of Applicants</Typography>
-        </Paper>
+        <Box className="page-container">
+            <Box className="page-header">
+                <Typography variant="h4" className="page-title-main">Reports</Typography>
+                <Typography variant="h5" className="page-title-sub">List of Applicants</Typography>
+            </Box>
+            <Box className="card-container">
+                {/* Content goes here */}
+            </Box>
+        </Box>
     );
 }
 
