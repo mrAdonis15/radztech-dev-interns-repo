1f258133d7bd97403f4fc8a4f0123c2c59f57d28
krt8 <kurtlawrence.b.manandig@isu.edu.ph>
2026-04-08 10:39:12 +0800

    fix the logo in login

--- src/DEV-INTERNS/Evaluation/api/endpoints.js
+++ src/DEV-INTERNS/Evaluation/api/endpoints.js
@@ -1,6 +1,9 @@
 export const ENDPOINT = "/api/lib/sub";
+export const SelectTemplate = "/api/trans/jcd/d2499174-a162-4873-8f6c-8ddaf144488e/templates";
 
 export const SUB_LIST_KEYS = {
   interns: "sub1",
   evaluators: "sub2",
 };
+
+export const SELECT_TEMPLATE = {};
